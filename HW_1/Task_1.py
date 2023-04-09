5b483ea5aa487a1e0d746d49148577b2a8b4abf5
MikhailTurusin <124387496+MikhailTurusin@users.noreply.github.com>
2023-04-09 22:36:45 +0300

    Update HW_1/Task_1.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- HW_1/Task_1.py
+++ HW_1/Task_1.py
@@ -10,7 +10,7 @@ number = int(input("Введите число: "))
 sum = 0
 while number > 0:
     sum += number % 10
-    number = number//10
+    number //= 10
 print(sum)
 
 # Второй вариант решения через строки:
